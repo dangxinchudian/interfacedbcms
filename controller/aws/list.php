7948dfd8867516cfcc6e5d9c75fff7fe888f2895
Twwy <zje2008@qq.com>
2013-05-15 11:49:01 +0800

    a

--- controller/aws/list.php
+++ controller/aws/list.php
@@ -20,8 +20,10 @@
 	$result = $siteModel->siteList($user_id, $start, $limit, 0);
 	$count = $siteModel->siteCount($user_id, 0);
 	foreach ($result as $key => $value) {
-		$result[$key]['general'] = $awsModel->general(date('Ym'), $value['site_id']);
-
+		$general = $awsModel->general(date('Ym'), $value['site_id']);
+		$result[$key]['visits'] = (isset($general['visits'])) ? $general['visits'] : 0;
+		$result[$key]['visits_unique'] = (isset($general['visits_unique'])) ? $general['visits_unique'] : 0;
+		$result[$key]['hits'] = (isset($general['hits'])) ? $general['hits'] : 0;
 	}
 	$array = array(
 		'page' => $page,
