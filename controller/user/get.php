ff578c75b525a405778cb6e2eee3f1c9f57f9757
Twwy <zje2008@qq.com>
2013-05-25 20:20:25 +0800

    a

--- controller/user/get.php
+++ controller/user/get.php
@@ -8,6 +8,9 @@
 	$info = $user->get($user_id);
 
 	if(empty($info)) json(false, '用户不存在');
+	if(isset($info['code_mobile'])) unset($info['code_mobile']);
+	if(isset($info['code_mail'])) unset($info['code_mail']);
+
 	json(true, $info);
 
 
