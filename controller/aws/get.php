b4003e1678bff62d758d86040a610da821c17c4e
Twwy <zje2008@qq.com>
2013-05-27 17:09:24 +0800

    a

--- controller/aws/get.php
+++ controller/aws/get.php
@@ -4,6 +4,8 @@
 	$user_id = $user->sessionCheck(function(){
 		json(false, '未登录');
 	});
+	$admin = $user->adminCheck();
+	if($admin) $user_id = 0;
 
 	$site_id = filter('site_id', '/^[0-9]{1,9}$/', 'siteID格式错误');
 
@@ -15,7 +17,7 @@
 
 	if(empty($info)) json(false, '站点不存在');
 	if($info['remove'] > 0) json(false, '站点已经被移除');
-	if($info['user_id'] != $user_id) json(false, '不允许操作他人站点');
+	if(!$admin) if($info['user_id'] != $user_id) json(false, '不允许操作他人站点');
 
 	$awsModel = model('aws');
 	$general = $awsModel->general(date('Ym'), $info['site_id']);
