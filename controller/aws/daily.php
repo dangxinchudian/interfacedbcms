21547f31f5c7f39fe8982c6225ab58c991130008
Twwy <zje2008@qq.com>
2013-05-27 17:13:16 +0800

    a

--- controller/aws/daily.php
+++ controller/aws/daily.php
@@ -4,6 +4,8 @@
 	$user_id = $user->sessionCheck(function(){
 		json(false, '未登录');
 	});
+	$admin = $user->adminCheck();
+	if($admin) $user_id = 0;
 
 	$site_id = filter('site_id', '/^[0-9]{1,9}$/', 'siteID格式错误');
 	$start_time = filter('start_time', '/^[0-9]{1,10}$/', '起始时间单位错误');
@@ -19,7 +21,7 @@
 
 	if(empty($info)) json(false, '站点不存在');
 	if($info['remove'] > 0) json(false, '站点已经被移除');
-	if($info['user_id'] != $user_id) json(false, '不允许操作他人站点');
+	if(!$admin) if($info['user_id'] != $user_id) json(false, '不允许操作他人站点');
 
 	$awsModel = model('aws');
 	$info = $awsModel->daily($info['site_id'], $start_time, $stop_time);
